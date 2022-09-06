e491f9a9de606d55e04c1fb6224d4d664efb6ad8
Devang <devngc@gmail.com>
2022-09-05 21:13:16 -0400

    feat(*): setup app

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const titleEl = document.getElementById("title");
+const activityEl = document.getElementById("activity");
+const triggerEl = document.getElementById("trigger");
+
+
+triggerEl.addEventListener("click", () => {
+    fetch("https://apis.scrimba.com/bored/api/activity")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            titleEl.textContent = "You gotta";
+            activityEl.textContent = data.activity;
+
+        });
+});
